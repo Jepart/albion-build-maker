60f2f8e29d5532568c31b64e05b75023bfefe1ff
Jean Carlos <jeancarlosjc210995@gmail.com>
2024-08-07 12:57:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -658,8 +658,9 @@ function getUrlParameters(url) {
 
 getUrlBuild = () => {
   var getParams = getUrlParameters(window.location.href);
-  if(getParams.length){
-  //http://127.0.0.1:3000/?bag=T4_BAG&head=T5_HEAD_PLATE_SET1|1|1||&cape=T4_CAPE&mainhand=T4_MAIN_CURSEDSTAFF|1|3||2&armor=T7_ARMOR_PLATE_SET1&offhand=T7_OFF_TOWERSHIELD_UNDEAD&potion=T5_POTION_REVIVE&shoes=T4_SHOES_PLATE_SET2&food=T4_FISH_FRESHWATER_ALL_COMMON
+  try {
+    
+  
   $.map(getParams, function (elementOrValue, indexOrKey) {
     var a = elementOrValue.split("|");
     if (indexOrKey == "title") {
@@ -673,6 +674,8 @@ getUrlBuild = () => {
       });
     }
   });
+} catch (error) {
+  u = 1
 }
 };
 
@@ -734,3 +737,5 @@ gerarLink = () => {
     linkBase + "/albion-build-maker/?" + param.join("&")
   );
 };
+
+// getUrlBuild()
